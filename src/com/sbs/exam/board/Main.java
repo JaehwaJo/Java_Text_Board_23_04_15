27e6b8b66fb8204236a7152222bfe003aeafb352
jaehwaJo <woghk2233@naver.com>
2023-04-15 13:53:04 +0900

    /usr/article/write 입력받기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -8,16 +8,28 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
+    int id = 1;
 
     while (true) {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
-      if(cmd.equals("exit")) {
+
+      if(cmd.equals("/usr/article/write")) {
+        System.out.println("게시물 등록");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+
+        System.out.println(id + "번 게시물이 등록되었습니다.");
+        id++;
+      }
+      else if(cmd.equals("exit")) {
+        System.out.println("== 프로그램 종료 ==");
         break;
       }
     }
     // System.out.println("입력 받은 명령어 : " + cmd);
-    System.out.println("== 프로그램 종료 ==");
 
     sc.close();
   }
