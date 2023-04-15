78ee89888326746e91a0b25b57e484e3d5de81f2
jaehwaJo <woghk2233@naver.com>
2023-04-15 14:49:35 +0900

    입력받은 게시물 정보를 Article 객체에 저장 후 객체 변수 출력

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -8,21 +8,29 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    int id = 1;
+    int articleLastId = 1;
 
     while (true) {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
 
       if(cmd.equals("/usr/article/write")) {
+        Article article = new Article();
+
         System.out.println("게시물 등록");
         System.out.printf("제목 : ");
         String title = sc.nextLine();
         System.out.printf("내용 : ");
         String body = sc.nextLine();
 
+        int id = articleLastId;
+        articleLastId++;
+
+        article.id = id;
+        article.title = title;
+        article.body = body;
+
         System.out.println(id + "번 게시물이 등록되었습니다.");
-        id++;
       }
       else if(cmd.equals("exit")) {
         System.out.println("== 프로그램 종료 ==");
@@ -33,4 +41,10 @@ public class Main {
 
     sc.close();
   }
+}
+
+class Article {
+  int id;
+  String title;
+  String body;
 }
