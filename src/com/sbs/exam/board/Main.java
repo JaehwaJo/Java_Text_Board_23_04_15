cab62073a99d9bdea06fd6954c95e7233de3334a
jaehwaJo <woghk2233@naver.com>
2023-04-15 13:05:48 +0900

    명령어 입력 받기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -1,7 +1,16 @@
 package com.sbs.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
+    System.out.printf("명령) ");
+    String cmd = sc.nextLine();
+    System.out.println("입력 받은 명령어 : " + cmd);
+    System.out.println("== 프로그램 종료 ==");
   }
 }
