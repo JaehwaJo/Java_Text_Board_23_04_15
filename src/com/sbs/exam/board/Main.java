fda627d5c8e09cad75b92c03db3c5b2a908f6d13
jaehwaJo <woghk2233@naver.com>
2023-04-16 15:30:22 +0900

    C:/Program Files/Git/usr/article/list 구현

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -1,5 +1,9 @@
 package com.sbs.exam.board;
 
+import javax.swing.plaf.IconUIResource;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,7 +12,8 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    int articleLastId = 1;
+    List<Article> articles = new ArrayList<>();
+    TestArticle(articles);
 
     while (true) {
       System.out.printf("명령) ");
@@ -21,22 +26,50 @@ public class Main {
         System.out.printf("내용 : ");
         String body = sc.nextLine();
 
-        int id = articleLastId;
-        articleLastId++;
+        int id = articles.size() + 1;
 
-        Article article = new Article(id, title, body);
+        articles.add(new Article(id, title, body));
+        Article article = articles.get(id - 1);
 
         System.out.println("생성 된 게시물 객체 : " + article);
         System.out.println(id + "번 게시물이 등록되었습니다.");
+      }
+      else if (cmd.equals("/usr/article/detail")) {
+        int id = articles.size() - 1;
+        Article article = articles.get(id);
+
+        System.out.println("== 게시물 상세 보기 ==");
+        System.out.println("번호 : " + article.id);
+        System.out.println("제목 : " + article.title);
+        System.out.println("내용 : " + article.body);
+      }
+      else if (cmd.equals("/usr/article/list")) {
+        System.out.println("== 게시물 리스트 ==");
+        System.out.println("--------------");
+        System.out.println("번호  /  제목");
+        System.out.println("--------------");
+        for (int i = articles.size() - 1; i >= 0; i--) {
+          Article article = articles.get(i);
+          System.out.printf("%d  /  %s\n", article.id, article.title);
+        }
       }
       else if(cmd.equals("exit")) {
         System.out.println("== 프로그램 종료 ==");
         break;
       }
+      else {
+        System.out.println(cmd + "는 올바른 명령어가 아닙니다.");
+      }
     }
     // System.out.println("입력 받은 명령어 : " + cmd);
 
     sc.close();
+  }
+
+  static void TestArticle(List<Article> articles) {
+    articles.add(new Article(1, "제목1", "내용1"));
+    articles.add(new Article(2, "제목2", "내용2"));
+    articles.add(new Article(3, "제목3", "내용3"));
   }
 }
 
@@ -44,6 +77,7 @@ class Article {
   int id;
   String title;
   String body;
+  public Article () {}
 
   public Article(int id, String title, String body) {
     this.id = id;
